07fcad200e523033f11f24eb9f8dec7eae21357e
Daniel Platt <dgplatt@berkeley.edu>
2019-09-04 16:39:41 -0700

    forgot move

---
+++ cards/Move.java
@@ -0,0 +1,37 @@
+package cards;
+public class Move {
+    int depth;
+    int from;
+    int to;
+    int value;
+    public Move (int depth, int from, int to, int value) {
+        this.depth = depth;
+        this.from = from;
+        this.to = to;
+        this.value = value;
+    }
+    public Move (int depth, int from, int to) {
+        this.depth = depth;
+        this.from = from;
+        this.to = to;
+        this.value = 0;
+    }
+    public int depth() {
+        return this.depth;
+    }
+    public int from() {
+        return this.from;
+    }
+    public int to() {
+        return this.to;
+    }
+    public int value() {
+        return this.value;
+    }
+    public String to_String() {
+        return ("  ||  depth : " + this.depth + "  ||  from : " + this.from + "  ||  to : " + this.to + "\n");
+    }
+    public void print(){
+        System.out.print(this.to_String());
+    }
+}
